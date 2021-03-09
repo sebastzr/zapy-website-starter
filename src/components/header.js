57acfe143abad29fb241d342ed384520c22aad71
Sebastian Zapata <sebastzr@utp.edu.co>
2021-03-09 15:37:02 -0500

    styles: header responsive and instagram components styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
+import headerStyles from "./header.module.scss"
 
 export default function Header() {
-  return <header>Header</header>
+  const [Header, setHeader] = useState(true)
+  const [NavList, setNavList] = useState(true)
+
+  const headerAnimation = () => {
+    if (window.scrollY >= 153) {
+      setHeader(false)
+    } else {
+      setHeader(true)
+    }
+  }
+
+  const showNavList = () => {
+    setNavList(!NavList)
+  }
+
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", headerAnimation)
+  }
+
+  return (
+    <header
+      className={
+        Header
+          ? headerStyles.header
+          : `${headerStyles.header} ${headerStyles.active}`
+      }
+    >
+      <nav
+        className={
+          NavList
+            ? headerStyles.navbar
+            : `${headerStyles.navbar} ${headerStyles.active}`
+        }
+      >
+        <div
+          onKeyDown={() => showNavList()}
+          role="link"
+          tabIndex="0"
+          onClick={() => showNavList()}
+          className={headerStyles.hamburger}
+        >
+          <div className={headerStyles.line}></div>
+          <div className={headerStyles.line}></div>
+          <div className={headerStyles.line}></div>
+        </div>
+        <div
+          onKeyDown={() => showNavList()}
+          role="link"
+          tabIndex="0"
+          onClick={() => showNavList()}
+          className={headerStyles.close}
+        >
+          &#10006;
+        </div>
+        <ul
+          className={
+            NavList
+              ? headerStyles.navList
+              : `${headerStyles.navList} ${headerStyles.active}`
+          }
+        >
+          <li className={headerStyles.navLink}>INICIO</li>
+          <li>LINK</li>
+          <li>LINK</li>
+          <li>LINK</li>
+          <li>LINK</li>
+          <li>LINK</li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react"
+import instagramStyles from "./instagram.module.scss"
 import _get from "lodash/get"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
-export default function News() {
+export default function Instagram() {
   const [posts, setPosts] = useState([])
 
   const axios = require("axios").default
@@ -9,7 +12,7 @@ export default function News() {
   useEffect(() => {
     axios
       .get(
-        `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=${process.env.GATSBY_INSTAGRAM_TOKEN}&limit=7`
+        `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type&access_token=${process.env.GATSBY_INSTAGRAM_TOKEN}&limit=7`
       )
       .then(function (response) {
         setPosts(_get(response, "data.data"))
@@ -19,11 +22,73 @@ export default function News() {
       })
   }, [])
 
+  const data = useStaticQuery(graphql`
+    query Instagram {
+      instagramTitle: file(relativePath: { eq: "instagram.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <div>
-      {posts.map((item, i) => {
-        return <div>{/* Your html here */}</div>
-      })}
+    <div className={instagramStyles.container}>
+      <div className={instagramStyles.title}>
+        <Img fluid={data.instagramTitle.childImageSharp.fluid} />
+      </div>
+      <div className={instagramStyles.posts}>
+        {posts.map((item, i) => {
+          if (item.media_type === "VIDEO") {
+            return (
+              <div className={instagramStyles.post} key={`post-${i}`}>
+                <iframe
+                  className={instagramStyles.img}
+                  src={item.media_url}
+                  frameborder="0"
+                  width="250px"
+                  height="250px"
+                  title={`insta-video-${i}`}
+                ></iframe>
+                <div className={instagramStyles.middle}>
+                  <a
+                    className={instagramStyles.text}
+                    href={item.permalink}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Ver publicación
+                  </a>
+                </div>
+              </div>
+            )
+          } else {
+            return (
+              <div className={instagramStyles.post} key={`post-${i}`}>
+                <img
+                  className={instagramStyles.img}
+                  src={item.media_url}
+                  alt={`Post instagram ${i}`}
+                  width="250px"
+                  height="250px"
+                />
+                <div className={instagramStyles.middle}>
+                  <a
+                    className={instagramStyles.text}
+                    href={item.permalink}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Ver publicación
+                  </a>
+                </div>
+              </div>
+            )
+          }
+        })}
+      </div>
     </div>
   )
 }
